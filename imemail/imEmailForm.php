39e319395644380e7b63fc72f777b5065a3eea1b
Bidhan Pokhrel <gotobrses@gmail.com>
2021-06-22 11:21:55 +0545

    changes in submit form

--- imemail/imEmailForm.php
+++ imemail/imEmailForm.php
@@ -4,7 +4,8 @@ if(substr(basename($_SERVER['PHP_SELF']), 0, 11) == "imEmailForm") {
 	$form = new ImForm();
 	$form->setField('Your Name', @$_POST['imObjectForm_47_1'], '', false);
 	$form->setField('Your E-mail', @$_POST['imObjectForm_47_2'], '', false);
-	$form->setField('Message', @$_POST['imObjectForm_47_3'], '', false);
+	$form->setField('Your Phone', @$_POST['imObjectForm_47_3'], '', false);
+	$form->setField('Message', @$_POST['imObjectForm_47_4'], '', false);
 
 	if(@$_POST['action'] != 'check_answer') {
 		if(!isset($_POST['imJsCheck']) || $_POST['imJsCheck'] != '3ADC14875A0AFBAAC46BFAFBEDA866C6' || (isset($_POST['imSpProt']) && $_POST['imSpProt'] != ""))
