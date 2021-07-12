b1d2f463aceb3c14d83d6e9ba1ade162d3fee4d3
brses <75085660+brses@users.noreply.github.com>
2021-07-12 20:01:12 +0545

    supervisor added

--- captcha/x5captcha.php
+++ captcha/x5captcha.php
@@ -1,7 +1,7 @@
 <?php
 include("../res/x5engine.php");
-$nameList = array("dms","s6a","5ap","7fd","sl4","sgg","ghd","n8f","unl","6tu");
-$charList = array("R","G","U","3","L","G","C","N","3","U");
+$nameList = array("dfp","yvd","378","cu2","j7a","5n3","u7a","hjh","vjx","xh5");
+$charList = array("W","R","N","C","J","R","A","F","7","X");
 $cpt = new X5Captcha($nameList, $charList);
 //Check Captcha
 if ($_GET["action"] == "check")
